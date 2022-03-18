35bf962671614f5fc86b946a6b18a97f47a7d56a
hyunmin <choihm9903@naver.com>
2022-03-18 15:30:06 +0900

    week3: index.html

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
+
 def index(request):
-    return render(request, 'blog/index.html')
+    posts = Post.objects.all().order_by('-pk')
+    return render(request, 'blog/index.html', {'posts':posts})
