a7610bf1c4f5df4a3c65ee9d74c1f97cfc800acf
hyunmin <choihm9903@naver.com>
2022-03-27 13:29:44 +0900

    feat: static image

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,16 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True, null=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     # author: 추후 작성 예정
 
+    # 모델 메소드 정의(오버라이딩)
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]    [{self.title}]'
+
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
