145bcbcd90557a3a2b9f6f0fd0a2d50bee6271e0
hyunmin <choihm9903@naver.com>
2022-04-08 17:42:02 +0900

    week6: tag

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from blog import views
 urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
-    path('category/<str:slug>/', views.cagegories_page)
+    path('category/<str:slug>/', views.cagegories_page),
+    path('tag/<str:slug>/', views.show_tag_posts)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from blog.models import Post, Category
+from blog.models import Post, Category, Tag
 
 
 class PostList(ListView):
@@ -37,6 +37,17 @@ def cagegories_page(request, slug):
         'category_less_post_count' : Post.objects.filter(category=None).count(),
         'category' : category,
         'post_list' : post_list
+    }
+    return render(request, 'blog/post_list.html', context)
+
+def show_tag_posts(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+    context = {
+        'categories': Category.objects.all(),
+        'category_less_post_count': Post.objects.filter(category=None).count(),
+        'tag': tag,
+        'post_list': post_list
     }
     return render(request, 'blog/post_list.html', context)
 
