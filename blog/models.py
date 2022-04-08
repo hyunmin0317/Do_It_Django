bd70f381d3af20e8a303e8fa4ea1c698531b1b89
hyunmin <choihm9903@naver.com>
2022-04-08 15:35:54 +0900

    week6: category

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,6 @@ class Post(models.Model):
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     # 모델 메소드 정의(오버라이딩)
@@ -25,3 +24,9 @@ class Post(models.Model):
         return os.path.basename(self.file_upload.name)
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
