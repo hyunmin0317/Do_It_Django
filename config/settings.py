74cf671489d3f982fdb9b4efc90ac309752acc7a
hyunmin.0317 <hyunmin.0317@navercorp.com>
2022-07-27 13:48:19 +0900

    feat: PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -82,10 +82,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-LOCAL_SQLITE='sqlite:///' + os.path.abspath(os.path.join(BASE_DIR, 'db.sqlite3'))
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(default=LOCAL_SQLITE)
-
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get("SQL_PORT", '5432'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
