724789349033413f3e45b06863ab6790753229a0
hyunmin.0317 <hyunmin.0317@navercorp.com>
2022-07-27 11:49:50 +0900

    feat: docker-setting

--- config/settings.py
+++ config/settings.py
@@ -21,16 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(o)$^apjv$^kml25etux0-0l0e!c)uqdr1x9__l(c_@oygnvai'
+SECRET_KEY = os.environ.get('SECRET_KEY', '(o)$^apjv$^kml25etux0-0l0e!c)uqdr1x9__l(c_@oygnvai')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
