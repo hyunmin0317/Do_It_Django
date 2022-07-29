c5903a41e53a7cb3086dd662904c8537afcd080e
hyunmin.0317 <hyunmin.0317@navercorp.com>
2022-07-29 11:36:32 +0900

    feat: Nginx

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
